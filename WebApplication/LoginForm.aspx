f7dea1f3368d9eb72218cc6952ed6aaeb73628c1
Yella-Bhavani <bhavaniyella123@gmail.com>
2020-09-23 10:02:21 +0530

    Add project files.

---
+++ WebApplication/LoginForm.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="LoginForm.aspx.cs" Inherits="WebApplication.LoginForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Login Page</title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            width: 370px;
+        }
+        .auto-style3 {
+            width: 376px;
+        }
+        .auto-style4 {
+            margin-left: 234px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <table class="auto-style1">
+        <tr>
+            <td class="auto-style2">
+                <asp:Label ID="lblusername" runat="server" Text="UserName"></asp:Label>
+            </td>
+            <td class="auto-style3">
+                <asp:TextBox ID="txtusername" runat="server"></asp:TextBox>
+            </td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td class="auto-style2">
+                <asp:Label ID="lblpwd" runat="server" Text="Password"></asp:Label>
+            </td>
+            <td class="auto-style3">
+                <asp:TextBox ID="txtpwd" TextMode="Password" runat="server"></asp:TextBox>
+            </td>
+            <td>
+                <asp:TextBox ID="lblMsg" runat="server" Width="338px"></asp:TextBox>
+            </td>
+        </tr>
+    </table>
+        <div>
+            <asp:Button ID="btnreg" runat="server" Text="Register" CssClass="auto-style4" OnClick="btnreg_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication/LoginForm.aspx.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication
+{
+    public partial class LoginForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblMsg.Visible = false;
+        }
+
+        protected void btnreg_Click(object sender, EventArgs e)
+        {
+            lblMsg.Visible = true;
+            lblMsg.Text = "Registration done successfully!!!";
+            {
+                if (txtusername.Text == "UserName")
+                {
+                    if (txtpwd.Text == "Password")
+                    {
+                        FormsAuthentication.RedirectFromLoginPage(txtusername.Text, false);
+                    }
+                    else
+                    {
+                        txtusername.Text = "Enter correct UserName";
+                    }
+                }
+                else
+                {
+                    txtpwd.Text = "Enter correct Password";
+                }
+            }
+        }
+    }
+    //public partial class Login : System.Web.UI.Page
+    //{
+    //    SqlConnection con;
+    //    SqlCommand cmd;
+    //    SqlDataReader srdr;
+    //    string constr = "server=US-5CG016FLQ5\\SQLSERVER;database=OurSecureDb;trusted_connection=true";
+
+
+    //    protected void Page_Load(object sender, EventArgs e)
+    //    {
+    //        Page.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+    //        lblMsg.Visible = false;
+    //    }
+    //    protected void btnSignIn_Click(object sender, EventArgs e)
+    //    {
+    //        lblMsg.Visible = true;
+    //        if ((txtUserName.Text == "sam") && (txtPwd.Text == "sam@1256"))
+    //        {
+    //            FormsAuthentication.RedirectFromLoginPage(txtUserName.Text, true);
+    //        }
+    //        else
+    //        {
+    //            lblMsg.Text = "Either user name or Password Incorrect !!!";
+    //        }
+
+    //try
+    //{
+    //    con = new SqlConnection(constr);
+    //    cmd = new SqlCommand
+    //    {
+    //        CommandText = "select UserName from UserLogin where UserName=@un and UserPwd=@upwd",
+    //        Connection = con
+    //    };
+    //    cmd.Parameters.AddWithValue("@un", txtusername.Text);
+    //    cmd.Parameters.AddWithValue("@upwd", txtpwd.Text);
+    //    con.Open();
+    //    srdr = cmd.ExecuteReader();
+    //    if (srdr.HasRows)
+    //    {
+    //        FormsAuthentication.RedirectFromLoginPage(txtusername.Text, true);
+    //    }
+    //    else
+    //    {
+    //        lblMsg.Text = "Login Failed, Invalid Credential";
+    //    }
+
+    //}
+    //catch (Exception ex)
+    //{ lblMsg.Text = "Error" + ex.Message; }
+    //finally
+    //{ con.Close();
+    //  Console.ReadKey();
+    //}
+}
+    
+
